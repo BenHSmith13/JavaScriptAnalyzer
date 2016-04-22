b8885fe340a5efe9ee58138d37c4e21042d70e93
dittonjs <jditton.atomic@gmail.com>
2016-04-22 11:04:59 -0600

    makes tokens right

--- tokenizer.py
+++ tokenizer.py
@@ -1,14 +1,14 @@
 keywords = [
-    "var", 
-    "function", 
-    "if", 
-    "else", 
-    "break", 
-    "switch", 
-    "case", 
-    "return", 
-    "while", 
-    "for", 
+    "var",
+    "function",
+    "if",
+    "else",
+    "break",
+    "switch",
+    "case",
+    "return",
+    "while",
+    "for",
     "let",
     "then",
     "this",
@@ -23,26 +23,12 @@ def tokenize(words):
         word = word_tup[0]
         if word in keywords:
             tokens.append(("KEYWORD", word, word_tup[1]))
-        elif word in [" ", "\t"]:
-            tokens.append(("WHITESPACE", word, word_tup[1]))
         elif word in [";", "\n"]:
-            tokens.append(("DELIMITER", word, word_tup[1]))
-        elif word == "(":
-            tokens.append(("OPEN_PARAM", word, word_tup[1]))
-        elif word == ")":
-            tokens.append(("CLOSE_PARAM", word, word_tup[1]))
-        elif word == "{":
-            tokens.append(("OPEN_BLOCK", word, word_tup[1]))
-        elif word == "}":
-            tokens.append(("CLOSE_BLOCK", word, word_tup[1]))
-        elif word == "[":
-            tokens.append(("OPEN_SUBSCRIPT", word, word_tup[1]))
-        elif word == "]":
-            tokens.append(("CLOSE_SUBSCRIPT", word, word_tup[1]))
-        elif word in "=!+-/*.":
-            tokens.append(("OPERATOR", word, word_tup[1]))
+            tokens.append(("END_MARKER", word, word_tup[1]))
+        elif word in "()\{\}[]=!+-/*.":
+            tokens.append(("PUNCT", word, word_tup[1]))
         else:
-            refined_token = refiner_and_lexical_error_parser(("IDENT", word, word_tup[1]))
+            refined_token = refiner_and_lexical_error_parser(("ID", word, word_tup[1]))
             tokens.append(refined_token)
     return tokens
 
@@ -52,20 +38,20 @@ def refiner_and_lexical_error_parser(token):
     num_chars = "0123456789"
     word = token[1]
     # only get the IDENT types, the others are what they are because their word matches something exactly already
-    if token[0] == "IDENT":
+    if token[0] == "ID":
         # handle strings
         if "\"" in word or "'" in word:
             if (word[0] == "\"" and word[-1] == "\"") or (word[0] == "'" and word[-1] == "'"):
-                return "STR_LITERAL", word, token[2]
+                return "LIT", word, token[2]
             else:
                 raise LexicalError("Lexical error at line: " + str(token[2]) + ", invalid string literal: " + word)
         # handle numbers, TODO: figure our floating point
         elif word[0] in num_chars:
             check_valid_number(word, token[2])
-            return "INT_LITERAL", word, token[2]
+            return "LIT", word, token[2]
         # handle booleans
         elif word == "true" or word == "false":
-            return "BOOL_LITERAL", word, token[2]
+            return "LIT", word, token[2]
         # put everything else back the way it was because
         else:
             return token
